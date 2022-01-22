75ba54112e2dc9a76154f185169fc60267a61681
Eric Ervin <ericcervin@gmail.com>
2022-01-21 23:56:41 -0500

    added pg029.rkt

---
+++ pg029.rkt
@@ -0,0 +1,23 @@
+#lang sketching
+(define (setup)
+  (size 480 120)
+  (smoothing 'smoothed)
+  (background 200)
+  (no-loop)
+  (no-stroke)
+  )
+
+(define (draw)
+  (begin-shape)
+  (fill 153 176 180)
+  (vertex 180 82)
+  (vertex 207 36)
+  (vertex 214 63)
+  (vertex 407 11)
+  (vertex 412 30)
+  (vertex 219 82)
+  (vertex 226 109)
+  (end-shape 'close)
+  )
+
+  
