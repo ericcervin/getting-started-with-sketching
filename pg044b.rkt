d65bf8b171e74408cc0cfdf9e9929c9004f88845
Eric Ervin <ericcervin@gmail.com>
2022-02-08 23:53:03 -0500

    added pg044b.rkt

---
+++ pg044b.rkt
@@ -0,0 +1,18 @@
+#lang sketching
+(define (setup)
+  (size 480 120)
+  (smoothing 'smoothed)
+  (background 200)
+  (no-loop)
+  )
+
+(define (draw)
+  (background 0)
+  (fill 255 140)
+  (no-stroke)
+  (for ([y (in-range 0 (+ height 1) 40)])
+      (for ([x (in-range 0 (+ width 1) 40)])
+        (ellipse x y 40 40)))
+  )
+
+  
